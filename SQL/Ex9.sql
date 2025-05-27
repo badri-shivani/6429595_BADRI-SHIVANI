78bb3b0655a02abf6e4fb2d7ef7c6829997f987a
badri-shivani <badrishivani5@gmail.com>
2025-05-27 18:27:48 +0530

    Create Ex9.sql

---
+++ SQL/Ex9.sql
@@ -0,0 +1,10 @@
+SELECT 
+    u.full_name AS organizer_name,
+    e.status,
+    COUNT(e.event_id) AS event_count
+FROM 
+    Events e
+JOIN 
+    Users u ON e.organizer_id = u.user_id
+GROUP BY 
+    e.organizer_id, e.status;
